68489ca2b77a4c1d02302ea68b8c5979ce88606c
Brentably <Brent@pabnet.com>
2022-12-13 20:36:22 -0500

    added connect wallet button

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */ // eslint-disable-line
+/** @type {import('next').NextConfig} */ 
 
 const nextConfig = {
   reactStrictMode: true,
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Web3OnboardProvider, init } from '@web3-onboard/react'
+import injectedModule from '@web3-onboard/injected-wallets'
+import walletConnectModule from '@web3-onboard/walletconnect'
+
+
+const rpcUrl = 'https://polygon-mumbai.g.alchemy.com/v2/LlPfIiQ_9R3vvvqY5HOadGN68ej0_I9z'
+
+const injected = injectedModule()
+const walletConnect = walletConnectModule()
+const web3onboard = init({
+  wallets: [injected, walletConnect],
+  chains: [
+    {
+      id: 80001,
+      token: 'MATIC',
+      label: 'Polygon Mumbai',
+      rpcUrl
+    }
+  ]
+})
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
