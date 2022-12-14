01a51a63c9b3c39f2535ca2c30d9752f8bc491f2
Brentably <Brent@pabnet.com>
2022-12-14 15:10:23 -0500

    new contracts

---
+++ lib/contracts/address.ts
@@ -0,0 +1,6 @@
+
+import LuckyLens from './LuckyLens.json'
+
+type localContract = [string, any[]]
+
+export const LuckyLensMumbai:localContract = ["0xd4Bf1e2006D31D7E20A52D1a9a311ae0C1F3a102", LuckyLens.abi]
