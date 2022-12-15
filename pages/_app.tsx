31f9f0e9f7d1f8f7d1f2018ec1639088a073ce1e
Brentably <Brent@pabnet.com>
2022-12-14 19:35:14 -0500

    Revert "issue in vercel build so im testing"
    This reverts commit 97bb133c61c1a06c8707ad8eb304376ddde9a2b1.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,8 +10,8 @@ import walletConnectModule from '@web3-onboard/walletconnect'
 
 export default function App({ Component, pageProps }: AppProps) {
   return ( 
-  
+  <Web3OnboardProvider web3Onboard={web3Onboard}>
     <Component {...pageProps} /> 
-
+  </Web3OnboardProvider>
   )
 }
