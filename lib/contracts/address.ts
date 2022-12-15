a31365b17bb7e94c991c4e2b754b54dff3f001f4
Brentably <Brent@pabnet.com>
2022-12-15 14:54:23 -0500

    updated contracts again

--- lib/contracts/address.ts
+++ lib/contracts/address.ts
@@ -5,14 +5,14 @@ import LuckyLens from './LuckyLens.json'
 
 // exports ethers contract that can be connected to a signer with contract.connect(Signer)
 // in the future can pre-connect to app's provider in here if read-only calls are prevalent in the app
-export const LuckyLensMumbai:Contract = new ethers.Contract("0x8649B353E95801A611554f447ff1Db72C7F00BB0", LuckyLens.abi)
+export const LuckyLensMumbai:Contract = new ethers.Contract("0x848024B671AA7CEc108a2524d7357A55bBFD10E9", LuckyLens.abi)
 
 
 // getting all raffles rn. not filtering for live raffles or anything.
 export const getRaffles = async(address: string, provider: ethers.providers.Web3Provider):Promise<any[]> => {
 
 const LuckyLens = LuckyLensMumbai.connect(provider)
-const 
+const postRaffleFilter = LuckyLens.filters.postRaffle()
 
 return []
 }
